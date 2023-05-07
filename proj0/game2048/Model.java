4228803d74ce61e7906c76f9bcdbacceaa2bb453
yh2527 <110083173+yh2527@users.noreply.github.com>
2023-05-07 15:06:50 -0700

    solved p1 and p2

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int r=0;r<b.size();r+=1){
+            for (int c=0;c<b.size();c+=1){
+                if (b.tile(c,r)==null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -148,6 +155,16 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (int r=0;r<b.size();r+=1){
+            for (int c=0;c<b.size();c+=1){
+                if (b.tile(c,r)==null){
+                    continue;
+                }
+                if (b.tile(c,r).value()==MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
