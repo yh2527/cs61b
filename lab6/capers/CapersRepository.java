22d3b1f63457c076fe233b02b00bc47d9dda014a
yh2527 <110083173+yh2527@users.noreply.github.com>
2023-06-02 22:30:57 -0700

    lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,46 +1,69 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
+
 import static capers.Utils.*;
 
-/** A repository for Capers 
+/**
+ * A repository for Capers
+ *
  * @author TODO
  * The structure of a Capers Repository is as follows:
- *
+ * <p>
  * .capers/ -- top level folder for all persistent data in your lab12 folder
- *    - dogs/ -- folder containing all of the persistent data for dogs
- *    - story -- file containing the current story
+ * - dogs/ -- folder containing all of the persistent data for dogs
+ * - story -- file containing the current story
+ * <p>
  *
- * TODO: change the above structure if you do something different.
  */
 public class CapersRepository {
-    /** Current Working Directory. */
+    /**
+     * Current Working Directory.
+     */
     static final File CWD = new File(System.getProperty("user.dir"));
 
-    /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    /**
+     * Main metadata folder.
+     */
+    static final File CAPERS_FOLDER = join(CWD, ".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
      * Remember: recommended structure (you do not have to follow):
-     *
+     * <p>
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdir();
+        File DOG_FOLDER = join(CAPERS_FOLDER, "dogs");
+        DOG_FOLDER.mkdir();
+        File STORY_FILE = join(CAPERS_FOLDER, "story");
+        try {
+            STORY_FILE.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println(CWD.getAbsolutePath());
+        //System.out.println(CAPERS_FOLDER.getAbsolutePath());
     }
 
     /**
      * Appends the first non-command argument in args
      * to a file called `story` in the .capers directory.
+     *
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File storyFile = join(CAPERS_FOLDER, "story");
+        String story = readContentsAsString(storyFile);
+        story += text ;
+        System.out.println(story);
+        story += "\n";
+        writeContents(storyFile, story);
     }
 
     /**
@@ -49,16 +72,21 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog d = new Dog(name, breed, age);
+        d.saveDog();
+        System.out.println(d.toString());
     }
 
     /**
      * Advances a dog's age persistently and prints out a celebratory message.
      * Also prints out the dog's information using toString().
      * Chooses dog to advance based on the first non-command argument of args.
+     *
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog d = Dog.fromFile(name);
+        d.haveBirthday();
+        d.saveDog();
     }
 }
--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,6 +1,7 @@
 package capers;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static capers.Utils.*;
 
@@ -40,7 +41,7 @@ public class Main {
         if (args.length == 0) {
             Utils.exitWithError("Must have at least one argument");
         }
-
+        //System.out.println("args: " + Arrays.toString(args));
         CapersRepository.setupPersistence();
         String text;
         switch (args[0]) {
@@ -52,11 +53,15 @@ public class Main {
             break;
         case "dog":
             validateNumArgs("dog", args, 4);
-            // TODO: make a dog
+            String name = args[1];
+            String breed = args[2];
+            Integer age = Integer.parseInt(args[3]);
+            CapersRepository.makeDog(name, breed, age);
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
-            // TODO: celebrate this dog's birthday
+            String dogName = args[1];
+            CapersRepository.celebrateBirthday(dogName);
             break;
         default:
             exitWithError(String.format("Unknown command: %s", args[0]));
